c56fae167efb62d98bb4aea5133c6d3cc7936207
vugar <abdullayev1.618@hotmail.com>
2018-08-28 10:40:04 +0400

    tab component replaced

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {AuthService} from '../auth/auth.service';
-
+import {transition, trigger, useAnimation} from '@angular/animations';
+import {flipInY } from 'ngx-animate';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('flipInY', [transition('* => *', useAnimation(flipInY))])
+  ],
 })
 export class HomeComponent implements OnInit {
   authenticated: boolean;
   links = [
-    {path: '/link-to-qr', label: 'Link To QR Code' },
-    {path: '/qr-to-link', label: 'QR To URL Link' }
+    {path: '/link-to-qr', label: 'Link To QR' },
+    {path: '/qr-to-link', label: 'QR To URL' }
   ];
+  flipInY = false;
   constructor(private router: Router, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit() {
@@ -22,4 +27,5 @@ export class HomeComponent implements OnInit {
       this.authenticated = res;
     });
   }
+  onRouteChange() {}
 }
